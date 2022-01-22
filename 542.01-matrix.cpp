fe07b142a3870fda6a5ce894edc263e3d899e281
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2022-01-23 06:48:12 +0900

    Cleanup solution for 542

--- 542.01-matrix.cpp
+++ 542.01-matrix.cpp
@@ -10,10 +10,7 @@ class Solution
 public:
     void updateDistance(vector<vector<int>> &mat, int x, int y, int distance)
     {
-        // printf("update distance, y: %i, x: %i\n", y, x);
         int step[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}}; // (y, x)
-
-        queue<pair<int, int>> q;
 
         // update four courners
         for (int i = 0; i < 4; i++)
@@ -23,19 +20,12 @@ public:
 
             // out of range
             if (currX < 0 || currX >= mat[0].size() || currY < 0 || currY >= mat.size())
-            {
                 continue;
-            }
-            // the direction we just came from
-            else if (mat[currY][currX] == -distance)
-            {
+
+            // the direction we just came from || already up-to-date
+            else if (mat[currY][currX] == -distance || mat[currY][currX] == -distance - 1)
                 continue;
-            }
-            // already updated
-            else if (mat[currY][currX] == -distance - 1)
-            {
-                continue;
-            }
+
             else if (mat[currY][currX] == 0)
             {
                 if (mat[y][x] != 0 && mat[y][x] != -1)
@@ -43,25 +33,13 @@ public:
                     mat[y][x] = -1;
                     updateDistance(mat, x, y, 1);
                 }
-
                 continue;
             }
             else if (mat[currY][currX] == 1 || mat[currY][currX] < -distance - 1)
             {
-                // printf("case 5\n");
                 mat[currY][currX] = -distance - 1;
-                q.push(make_pair(currX, currY));
-
-                // updateDistance(mat, currX, currY, distance + 1);
+                updateDistance(mat, currX, currY, distance + 1);
             }
-        }
-
-        while (!q.empty())
-        {
-            int currX = q.front().first;
-            int currY = q.front().second;
-            q.pop();
-            updateDistance(mat, currX, currY, distance + 1);
         }
     }
 
