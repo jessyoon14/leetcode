0c072d6df4b637a00a705b763cf65dcca9a0a749
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2022-01-23 06:45:08 +0900

    Working solutioin for 542

--- 542.01-matrix.cpp
+++ 542.01-matrix.cpp
@@ -8,84 +8,72 @@
 class Solution
 {
 public:
-    void updateDistance(vector<vector<int>> &mat, int x, int y)
+    void updateDistance(vector<vector<int>> &mat, int x, int y, int distance)
     {
-        int step[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}}; // (y, x)
+        // printf("update distance, y: %i, x: %i\n", y, x);
+        int step[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}}; // (y, x)
 
+        queue<pair<int, int>> q;
+
+        // update four courners
         for (int i = 0; i < 4; i++)
         {
+            int currX = x + step[i][1];
+            int currY = y + step[i][0];
 
-            int nextX = x + step[i][1];
-            int nextY = y + step[i][0];
-
-            if (nextX < 0 || nextX >= mat[0].size() || nextY < 0 || nextY >= mat.size())
-                continue;
-
-            int currDistance = getDistance(mat, nextX, nextY);
-            minDistance = minDistance < currDistance ? minDistance : currDistance;
-            if (minDistance == 0)
-                break;
-        }
-    }
-
-    int getDistance(vector<vector<int>> &mat, int x, int y)
-    {
-        if (mat[y][x] == 0)
-            return 0;
-
-        int minDistance = 20001;
-
-        if (mat[y][x] < 0)
-            return -mat[y][x];
-        else
-            mat[y][x] = -minDistance;
-
-        int step[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}}; // (y, x)
-
-        for (int i = 0; i < 4; i++)
-        {
-
-            int nextX = x + step[i][1];
-            int nextY = y + step[i][0];
-
-            if (nextX < 0 || nextX >= mat[0].size() || nextY < 0 || nextY >= mat.size())
-                continue;
-
-            int currDistance = getDistance(mat, nextX, nextY);
-            minDistance = minDistance < currDistance ? minDistance : currDistance;
-            if (minDistance == 0)
-                break;
-        }
-
-        for (int i = 0; i < 4; i++)
-        {
-
-            int nextX = x + step[i][1];
-            int nextY = y + step[i][0];
-
-            if (nextX < 0 || nextX >= mat[0].size() || nextY < 0 || nextY >= mat.size())
-                continue;
-            // need to update
-            if (mat[nextY][nextX] < 0 && mat[nextY][nextX] < -minDistance - 2)
+            // out of range
+            if (currX < 0 || currX >= mat[0].size() || currY < 0 || currY >= mat.size())
             {
-                mat[nextY][nextX] = -minDistance - 2;
-                updateDistance(mat, nextX, nextY);
+                continue;
+            }
+            // the direction we just came from
+            else if (mat[currY][currX] == -distance)
+            {
+                continue;
+            }
+            // already updated
+            else if (mat[currY][currX] == -distance - 1)
+            {
+                continue;
+            }
+            else if (mat[currY][currX] == 0)
+            {
+                if (mat[y][x] != 0 && mat[y][x] != -1)
+                {
+                    mat[y][x] = -1;
+                    updateDistance(mat, x, y, 1);
+                }
+
+                continue;
+            }
+            else if (mat[currY][currX] == 1 || mat[currY][currX] < -distance - 1)
+            {
+                // printf("case 5\n");
+                mat[currY][currX] = -distance - 1;
+                q.push(make_pair(currX, currY));
+
+                // updateDistance(mat, currX, currY, distance + 1);
             }
         }
 
-        mat[y][x] = -(minDistance + 1);
-        return minDistance + 1;
+        while (!q.empty())
+        {
+            int currX = q.front().first;
+            int currY = q.front().second;
+            q.pop();
+            updateDistance(mat, currX, currY, distance + 1);
+        }
     }
+
     vector<vector<int>> updateMatrix(vector<vector<int>> &mat)
     {
         int height = mat.size();
         int width = mat[0].size();
         for (int y = 0; y < height; y++)
             for (int x = 0; x < width; x++)
-                if (mat[y][x] > 0)
+                if (mat[y][x] == 0)
                 {
-                    printf("in updateMatrix, call getDistance\n");
-                    getDistance(mat, x, y);
+                    updateDistance(mat, x, y, 0);
                 }
 
         for (int y = 0; y < height; y++)
