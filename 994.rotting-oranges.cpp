a8c4a1d1a3f92559fb3b9af863d6e1405a286966
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2022-01-23 17:35:53 +0900

    Finish 994

--- 994.rotting-oranges.cpp
+++ 994.rotting-oranges.cpp
@@ -5,61 +5,72 @@
  */
 
 // @lc code=start
+
 class Solution {
    public:
+    void update(vector<vector<int>> &grid, vector<vector<int>> &dist, int i, int j) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int step[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        for (int k = 0; k < 4; k++) {
+            int x = j + step[k][1];
+            int y = i + step[k][0];
+
+            if (x >= 0 && x < cols && y >= 0 && y < rows && grid[y][x] == 1) {
+                if (dist[i][j] + 1 < dist[y][x]) {
+                    dist[y][x] = dist[i][j] + 1;
+                    update(grid, dist, y, x);
+                }
+            }
+        }
+    }
+
     int orangesRotting(vector<vector<int>> &grid) {
         int rows = grid.size();
         int cols = grid[0].size();
 
         const int UNREACHABLE = INT_MAX - 100000;
 
-        vector<vector<int>> dist(rows, vector<int>(cols, UNREACHABLE));  // 왜 -100000?
+        vector<vector<int>> dist(rows, vector<int>(cols, UNREACHABLE));
 
         int freshCount = 0;
-        // DP 1: top to bottom, left to right
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                // is rotten
-                if (grid[i][j] == 2)
+                if (grid[i][j] == 2) {
                     dist[i][j] = 0;
-
-                // is fresh
-                else if (grid[i][j] == 1) {
-                    freshCount++;
-                    if (i > 0)
-                        dist[i][j] = min(dist[i][j], dist[i - 1][j] + 1);
-                    if (j > 0)
-                        dist[i][j] = min(dist[i][j], dist[i][j - 1] + 1);
                 }
-                printf("dist[%i][%i]: %i\n", i, j, dist[i][j]);
+
+                if (grid[i][j] == 1)
+                    freshCount++;
             }
         }
 
         if (freshCount == 0)
             return 0;
 
-        printf("freshCount: %i\n", freshCount);
-
-        int maxTimeToRot = 0;
-
-        // DP 2: bottom to top, right to left
-        for (int i = rows - 1; i >= 0; i--) {
-            for (int j = cols - 1; j >= 0; j--) {
-                if (grid[i][j] == 1) {
-                    if (i < rows - 1)
-                        dist[i][j] = min(dist[i][j], dist[i + 1][j] + 1);
-                    if (j < cols - 1)
-                        dist[i][j] = min(dist[i][j], dist[i][j + 1] + 1);
-                    // this orange will never rot
-                    if (dist[i][j] == UNREACHABLE)
-                        return -1;
-                    maxTimeToRot = maxTimeToRot > dist[i][j] ? maxTimeToRot : dist[i][j];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    update(grid, dist, i, j);
                 }
-                printf("dist[%i][%i]: %i\n", i, j, dist[i][j]);
             }
         }
+
+        int maxTimeToRot = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1)
+                    maxTimeToRot = maxTimeToRot > dist[i][j] ? maxTimeToRot : dist[i][j];
+            }
+        }
+
+        if (maxTimeToRot == UNREACHABLE)
+            maxTimeToRot = -1;
 
         return maxTimeToRot;
     }
 };
+
 // @lc code=end
