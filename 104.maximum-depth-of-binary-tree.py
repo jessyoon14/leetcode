e260973c9cacffad1aaf512ea2333a4006f3be41
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2022-04-26 02:16:40 +0900

    add 104

--- 104.maximum-depth-of-binary-tree.py
+++ 104.maximum-depth-of-binary-tree.py
@@ -19,19 +19,20 @@
 #         self.right = right
 class Solution:
     # iterative solution with stack
-    # def maxDepth(self, root: Optional[TreeNode]) -> int:
-    #     stack = []
-    #     if root is not None:
-    #         stack.append((1, root))
 
-    #     depth = 0
-    #     while stack != []:
-    #         current_depth, root = stack.pop()
-    #         if root is not None:
-    #             depth = max(depth, current_depth)
-    #             stack.append((current_depth + 1, root.left))
-    #             stack.append((current_depth + 1, root.right))
-    #     return depth
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = []
+        if root is not None:
+            stack.append((1, root))
+
+        depth = 0
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(depth, current_depth)
+                stack.append((current_depth + 1, root.left))
+                stack.append((current_depth + 1, root.right))
+        return depth
 
     # BFS solution with queue
     # def maxDepth(self, root: Optional[TreeNode]) -> int:
