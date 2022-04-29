3d452ecd5a2e638e29a0f63332febd417b566e4e
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2022-04-30 03:39:04 +0900

    fix 366

--- 366.find-leaves-of-binary-tree.py
+++ 366.find-leaves-of-binary-tree.py
@@ -1,3 +1,80 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    #     """
+    #     DFS with sorting
+    #     """
+    #     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+    #         if not root:
+    #             return [[]]
+    #         acc = []
+
+    #         def rec(root):
+    #             if root is None:
+    #                 return -1
+    #             left_height = rec(root.left)
+    #             right_height = rec(root.right)
+    #             height = max(left_height, right_height) + 1
+    #             acc.append((height, root.val))
+    #             return height
+
+    #         rec(root)
+    #         acc.sort(key=lambda x:x[0])
+    #         result = []
+    #         for a in acc:
+    #             if len(result) - 1 < a[0]:
+    #                 result.append([])
+    #             result[a[0]].append(a[1])
+
+    #         return result
+
+    #     """
+    #     One pass DFS
+    #     """
+    #     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+    #         if not root:
+    #             return [[]]
+    #         res = []
+
+    #         def rec(root):
+    #             if root is None:
+    #                 return -1
+    #             left_height = rec(root.left)
+    #             right_height = rec(root.right)
+    #             height = max(left_height, right_height) + 1
+    #             if len(res) - 1 < height:
+    #                 res.append([])
+    #             res[height].append(root.val)
+    #             return height
+
+    #         rec(root)
+    #         return res
+
+    """
+    DFS
+    """
+#     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         output = collections.defaultdict(list)
+
+#         def dfs(node):
+#             if not node:
+#                 return 0
+#             left = dfs(node.left)
+#             right = dfs(node.right)
+#             layer = max(left, right)
+#             output[layer].append(node.val)
+#             return layer + 1
+
+#         dfs(root)
+
+#         return output.values()
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
